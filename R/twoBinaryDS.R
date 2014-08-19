67c958aa30be9959dfd002c6d7f977704f29ee8a
agaye <ag13748@it017324.(none)>
2014-08-19 16:05:59 +0100

    First commit.

---
+++ R/twoBinaryDS.R
@@ -0,0 +1,35 @@
+#'
+#' @title Compute cells frequencies for two dichotomous variables comparison
+#' @description helps to computes measures for two-group compaparison with two binary variables.
+#' @details generates a 2x2 table and puts the cell frequencies in a vector. Cell counts
+#' smaller than the allowed minimum count are replaced by 0. The ouput of this function
+#' can be used for example in the calculations of: log relative risk, log odds ratio and 
+#' risk difference.
+#' @param variables a list that holds the two variables.
+#' @apram data the dataframe that holds the variables.
+#' @return a numeric vector, the values 2x2 contingency table.
+#' @author Gaye, A.
+#' @export
+#'
+twoBinaryDS <- function(variables=NULL,data=NULL){
+  
+  # this filter sets the minimum number of observations that are allowed 
+  nfilter <- 5
+  
+  # allow for the variable to be accessed individually 
+  if(!(is.null(data))){ attach(data) }
+  mat <- table(eval(terms(formula)[[3]]), eval(terms(formula)[[2]]))
+  
+  # loop through the count and turn to 0 those < to the minimum cell count allowed
+  output <- c()
+  for(i in 1:2){
+    for(j in 1:2){
+      if(mat[j,i] < nfilter) {
+        output <- append(output, 0)
+      }else{
+        output <- append(output, mat[j,i])
+      }
+    }
+  }	  
+  return(output)
+}
---
+++ R/twoQtlDS.R
@@ -0,0 +1,36 @@
+#'
+#' @title Compute mean, sd and size for two quantitaive variables comparison
+#' @description helps to computes measures for two-group compaparison with two quantitative variables.
+#' @details generates a contingency table and puts the mean, sd and length values in a vector. 
+#' The ouput of this function can be used for example in the calculations of: raw mean difference,
+#' standardized mean difference and other measures.
+#' @param formula a formula that specifies the outcome and the covariate.
+#' @apram data the dataframe that holds the variables.
+#' @return a numeric vector, the mean, sd and lengths of each of the two vectors.
+#' @author Gaye, A.
+#' @export
+#'
+twoQtlDS <- function(variables=NULL,data=NULL){
+  
+  # this filter sets the minimum number of observations that are allowed 
+  nfilter <- 5
+  
+  # allow for the variable to be accessed individually 
+  if(!(is.null(data))){ attach(data) }
+  
+  # get the mean, sd and length
+  output <- c()
+  for(i in 1:2){
+    c1 <-  mean(variables[[i]], na.rm=TRUE)
+    c2 <-  sd(variables[[i]], na.rm=TRUE)
+    c3 <-  length(variables[[i]])
+    if(c3 < nfilter){
+      c1 <- NA
+      c2 <- NA
+      c3 <- NA
+    }
+    output <- append(output, c(c1,c2,round(c3)))
+  }
+
+  return(output)
+}
